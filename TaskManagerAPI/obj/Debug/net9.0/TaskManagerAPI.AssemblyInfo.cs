a1c70db4ce0b4eb083363cf47f7b0cc61040c019
hagit eizen <hagithalle@gmail.com>
2025-09-10 11:14:17 +0300

    Remove bin and obj from repository

--- TaskManagerAPI/obj/Debug/net9.0/TaskManagerAPI.AssemblyInfo.cs
+++ TaskManagerAPI/obj/Debug/net9.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b470e56bde7cc01825c0d2c9bb448c3c7ea71a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05ae38a15fe2502049b8232dc18a8d1aee3f561")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
